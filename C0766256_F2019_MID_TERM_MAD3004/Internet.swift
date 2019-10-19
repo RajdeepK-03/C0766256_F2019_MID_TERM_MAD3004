aea15e320d6d632c9ef099437298290b1e01e3b3
Rajdeep <chahal.rajdeep03@gmail.com>
2019-10-19 19:01:36 -0400

    Class Internet Created and Insurance Created

---
+++ C0766256_F2019_MID_TERM_MAD3004/Internet.swift
@@ -0,0 +1,9 @@
+//
+//  Internet.swift
+//  C0766256_F2019_MID_TERM_MAD3004
+//
+//  Created by macstudent on 2019-10-19.
+//  Copyright © 2019 macstudent. All rights reserved.
+//
+
+import Foundation
